c139badb9e5580c193472627dbbeb48a42f939ea
yashshri01 <125113743+yashshri01@users.noreply.github.com>
2024-02-20 21:24:45 +0530

    Demo.java

---
+++ Abstraction/using Interface /Demo.java
@@ -0,0 +1,11 @@
+package com.interfac;
+
+public interface Demo {
+	public void m1();
+	public void m2();
+}
+
+// Interface support 100% abstraction
+// only abstract method(un-implemented) means it doesn't contain implemented methhod 
+
+// abstraction + interface
