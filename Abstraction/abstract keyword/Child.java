02d28fe8bf19135ef46a69714a296e78e8d858e3
yashshri01 <125113743+yashshri01@users.noreply.github.com>
2024-02-20 21:20:43 +0530

    Child.java

--- Abstraction/abstract keyword/Child.java
+++ Abstraction/abstract keyword/Child.java
@@ -1,1 +1,17 @@
+package com.intro;
+
+public class Child extends Collage {
+
+	public void getDep() {
+		System.out.println("CSE");
+	}
+
+	public void getTeachername() {
+		System.out.println("abc faculty");
+	}
+
+}
+// to access abstract method from Parent class 
+//1) we need to make Child class abstract
+//2)Or we need to implement that abstract method i.e override
 
